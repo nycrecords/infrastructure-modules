14092eebe2fc7315bb207474c32d81908028012a
Joel Castillo <jocastillo@records.nyc.gov>
2021-05-05 10:18:10 -0400

    Update nsg_rules to use object

--- azurerm_network_security_group_rules/variables.tf
+++ azurerm_network_security_group_rules/variables.tf
@@ -10,5 +10,14 @@ variable "nsg_name" {
 
 variable "nsg_rules" {
   description = "Network Security Group Rules to create. Format should be <Rule Name> = [priority, direction, access, protocol, source_port_range, destination_port_ranges, source_address_prefix, destination_address_prefix]"
-  type = map(any)
+  type = object({
+    priority = string,
+    direction = string,
+    access = string,
+    protocol = string,
+    source_port_range = string,
+    destination_port_ranges = string,
+    source_address_prefix = string,
+    destination_address_prefix = string
+  })
 }
