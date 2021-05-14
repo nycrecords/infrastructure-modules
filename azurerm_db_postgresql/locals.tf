f8f26a7091d032b5de3458076db29a3c3468ff5b
Joel Castillo <jocastillo@records.nyc.gov>
2021-05-14 16:11:42 -0400

    Initial version of azurerm_db_postgresql (based on https://github.com/claranet/terraform-azurerm-db-postgresql)
    Co-authored-by: Gary Zhou <gzhou@records.nyc.gov>

---
+++ azurerm_db_postgresql/locals.tf
@@ -0,0 +1,16 @@
+locals {
+  name_prefix  = var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""
+  default_name = lower("${local.name_prefix}${var.app}-${module.azure-region.location_short}-${var.environment}")
+
+  postgresql_server_name = coalesce(var.custom_server_name, "${local.default_name}-postgresql")
+
+  default_tags = {
+    env   = var.environment
+  }
+
+  tier_map = {
+    "GeneralPurpose"  = "GP"
+    "Basic"           = "B"
+    "MemoryOptimized" = "MO"
+  }
+}
---
+++ azurerm_db_postgresql/variables.tf
@@ -0,0 +1,165 @@
+variable "environment" {
+  description = "Name of application's environnement"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the application ressource group, herited from infra module"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure location for Key Vault."
+  type        = string
+}
+
+variable "location_short" {
+  description = "Short string for Azure location."
+  type        = string
+}
+
+variable "name_prefix" {
+  description = "Optional prefix for PostgreSQL server name"
+  type        = string
+  default     = ""
+}
+
+variable "custom_server_name" {
+  type        = string
+  description = "Custom Server Name identifier"
+  default     = ""
+}
+
+variable "administrator_login" {
+  description = "PostgreSQL administrator login"
+  type        = string
+}
+
+variable "administrator_password" {
+  description = "PostgreSQL administrator password. Strong Password : https://docs.microsoft.com/en-us/sql/relational-databases/security/strong-passwords?view=sql-server-2017"
+  type        = string
+}
+
+variable "allowed_cidrs" {
+  type        = map(string)
+  description = "Map of authorized cidrs, must be provided using remote states cloudpublic/cloudpublic/global/vars/terraform.state"
+}
+
+variable "extra_tags" {
+  type        = map(string)
+  description = "Map of custom tags"
+  default     = {}
+}
+
+variable "tier" {
+  type        = string
+  description = "Tier for MySQL server sku : https://www.terraform.io/docs/providers/azurerm/r/mysql_server.html#tier Possible values are: GeneralPurpose, Basic, MemoryOptimized"
+  default     = "GeneralPurpose"
+}
+
+variable "capacity" {
+  type        = number
+  description = "Capacity for MySQL server sku : https://www.terraform.io/docs/providers/azurerm/r/mysql_server.html#capacity"
+  default     = 4
+}
+
+variable "auto_grow_enabled" {
+  description = "Enable/Disable auto-growing of the storage."
+  type        = bool
+  default     = false
+}
+
+variable "storage_mb" {
+  description = "Max storage allowed for a server. Possible values are between 5120 MB(5GB) and 1048576 MB(1TB) for the Basic SKU and between 5120 MB(5GB) and 4194304 MB(4TB) for General Purpose/Memory Optimized SKUs."
+  type        = number
+  default     = 5120
+}
+
+variable "backup_retention_days" {
+  description = "Backup retention days for the server, supported values are between 7 and 35 days."
+  type        = number
+  default     = 10
+}
+
+variable "geo_redundant_backup_enabled" {
+  description = "Turn Geo-redundant server backups on/off. Not available for the Basic tier."
+  type        = bool
+  default     = true
+}
+
+variable "postgresql_configurations" {
+  type        = map(string)
+  default     = {}
+  description = "PostgreSQL configurations to enable"
+}
+
+variable "postgresql_version" {
+  type        = string
+  default     = "11"
+  description = "Valid values are 9.5, 9.6, 10, 10.0, and 11"
+}
+
+variable "force_ssl" {
+  type        = bool
+  default     = true
+  description = "Force usage of SSL"
+}
+
+variable "vnet_rules" {
+  type        = map(string)
+  description = "Map of vnet rules to create"
+  default     = {}
+}
+
+variable "databases_names" {
+  description = "List of databases names"
+  type        = list(string)
+}
+
+variable "databases_charset" {
+  type        = map(string)
+  description = "Valid PostgreSQL charset : https://www.postgresql.org/docs/current/multibyte.html#CHARSET-TABLE"
+  default     = {}
+}
+
+variable "databases_collation" {
+  type        = map(string)
+  description = "Valid PostgreSQL collation : http://www.postgresql.cn/docs/9.4/collation.html - be careful about https://docs.microsoft.com/en-us/windows/win32/intl/locale-names?redirectedfrom=MSDN"
+  default     = {}
+}
+
+variable "enable_logs_to_storage" {
+  description = "Boolean flag to specify whether the logs should be sent to the Storage Account"
+  type        = bool
+  default     = false
+}
+
+variable "enable_logs_to_log_analytics" {
+  description = "Boolean flag to specify whether the logs should be sent to Log Analytics"
+  type        = bool
+  default     = false
+}
+
+variable "logs_storage_retention" {
+  description = "Retention in days for logs on Storage Account"
+  type        = number
+  default     = 30
+}
+
+variable "logs_storage_account_id" {
+  description = "Storage Account id for logs"
+  type        = string
+  default     = ""
+}
+
+variable "logs_log_analytics_workspace_id" {
+  description = "Log Analytics Workspace id for logs"
+  type        = string
+  default     = ""
+}
+
+variable "create_databases_users" {
+  description = "True to create a user named <db>_user per database with generated password and role db_owner."
+  type        = bool
+  default     = true
+}
