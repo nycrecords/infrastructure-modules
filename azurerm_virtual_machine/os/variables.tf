9b605e86b5b6dcb330bcf239d4c4c0352ddce743
Gary Zhou <gzhou@records.nyc.gov>
2021-11-29 14:29:24 -0500

    Set default RHEL version to 8.4

--- azurerm_virtual_machine/os/variables.tf
+++ azurerm_virtual_machine/os/variables.tf
@@ -7,7 +7,7 @@ variable "standard_os" {
   default = {
     "UbuntuServer"  = "Canonical,UbuntuServer,18.04-LTS"
     "WindowsServer" = "MicrosoftWindowsServer,WindowsServer,2019-Datacenter"
-    "RHEL"          = "RedHat,RHEL,8.2"
+    "RHEL"          = "RedHat,RHEL,8.4"
     "openSUSE-Leap" = "SUSE,openSUSE-Leap,15.1"
     "CentOS"        = "OpenLogic,CentOS,7.6"
     "Debian"        = "credativ,Debian,9"
