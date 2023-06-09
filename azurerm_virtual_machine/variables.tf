d92f041d212102b5cd04d025c1605a5f5de4a57f
Gary Zhou <gzhou@records.nyc.gov>
2023-06-09 16:00:40 -0400

    Update variables.tf
    Change ssh_key to ssh_key_file

--- azurerm_virtual_machine/variables.tf
+++ azurerm_virtual_machine/variables.tf
@@ -41,7 +41,7 @@ variable "extra_ssh_keys" {
   default     = []
 }
 
-variable "ssh_key" {
+variable "ssh_key_file" {
   description = "Path to the public key to be used for ssh access to the VM. Only used with non-Windows vms and can be left as-is even if using Windows vms. If specifying a path to a certification on a Windows machine to provision a linux vm use the / in the path versus backslash. e.g. c:/home/id_rsa.pub."
   type        = string
   default     = "~/.ssh/id_rsa.pub"
